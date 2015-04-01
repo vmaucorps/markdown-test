aab346788729005beb141329253c99359a2cd12b
Tony Cabaye <tonai59+github@gmail.com>
2015-04-01 09:58:05 +0200

    Create 04-syntax-highlighting.js

---
+++ Markdown syntax/02 - GitHub markdown/04-syntax-highlighting.js
@@ -0,0 +1,23 @@
+# Syntax highlighting
+
+## Javascript
+
+```javascript
+$('.js-contentToggle--menu').contentToggle({
+  globalClose: true,
+  beforeCallback: function() {
+    // Prevent opening the menu if it is already visible with CSS.
+    return this.isOpen || !this.$contents.is(':visible');
+  },
+  toggleOptions: {
+    duration: 0,
+    complete: function() {
+      // Remove the "display:none" inline style added by jQuery.
+      var $this = $(this);
+      if (!$this.is(':visible')) {
+        $this.css({display: ''});
+      }
+    }
+  }
+});
+```
